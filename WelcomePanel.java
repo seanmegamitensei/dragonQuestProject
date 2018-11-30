b4ce0e80058992679f670c835aafa6176503570c
seanmegamitensei <45500864+seanmegamitensei@users.noreply.github.com>
2018-11-30 17:28:24 -0600

    Update WelcomePanel.java

--- WelcomePanel.java
+++ WelcomePanel.java
@@ -1,5 +1,18 @@
 package test;
 
-public class WelcomePanel {
+import java.awt.*;
+import javax.swing.*;
+
+public class WelcomePanel extends JPanel{
+	public WelcomePanel() {
+		setPreferredSize(new Dimension(300, 75));
+	    setBackground(Color.black);
+	    
+	    JLabel label1 = new JLabel("Slime Time!\n" );
+	    JLabel label2 = new JLabel("Created By: Sean Lovett" );
+	    
+	    add(label1);
+	    add(label2);
+	}
 
 }
