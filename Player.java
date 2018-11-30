29070039dce2acb36da1ce1d61d4371bdb0ea7c1
seanmegamitensei <45500864+seanmegamitensei@users.noreply.github.com>
2018-11-30 17:58:22 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,30 +1,33 @@
 package test;
+import java.awt.event.ActionEvent;
 import java.util.Random;
 
 public class Player{
+	Player Hero = new Player("Hero", 50, 20);
+    Player Slime = new Player("Slime", 50, 20);
     private final String userName;
     private int hp;
-    private int sp;
+    private int mp;
 
-    public Player(String userName, int hp, int sp) {
+    public Player(String userName, int hp, int mp) {
         this.userName = userName;
         this.hp = hp;
-        this.sp = sp;
+        this.mp = mp;
     }
 
-    public String attack(Player enemy) {
+    public void attack(Player Slime) {
         Random rnd = new Random();
         int damage = rnd.nextInt(10) + 1;
-        enemy.dealDamage(damage);
-        return (getUserName() + " deals " + damage + " to " + enemy.getUserName()) +
-        (enemy.getUserName() + " has " + enemy.getHp() + " hit points left!");
+        Slime.dealDamage(damage);
+        System.out.println((getUserName() + " deals " + damage + " to " + Slime.getUserName()) +
+        (Slime.getUserName() + " has " + Slime.getHp() + " hit points left!"));
     }
-    public String useMagic(Player enemy) {
+    public void useMagic(Player Slime) {
         Random rnd = new Random();
-        int damage = rnd.nextInt(21) + 1;
-        enemy.dealMagicDamage(damage);
-        return (getUserName() + " casts Frizz and deals " + damage + " to " + enemy.getUserName()) +
-        (enemy.getUserName() + " has " + enemy.getHp() + " hit points left!");
+        int damage = rnd.nextInt(21) + 10;
+        Slime.dealMagicDamage(damage);
+        System.out.println ((getUserName() + " casts Frizz and deals " + damage + " to " + Slime.getUserName()) +
+        (Slime.getUserName() + " has " + Slime.getHp() + " hit points left!"));
     }
 
     public void dealDamage(int damage) {
@@ -36,23 +39,29 @@ public class Player{
     }
     public void dealMagicDamage(int damage) {
     	hp -= damage;
-    	if (sp >= 5) {
+    	if (mp >= 5) {
     		if (hp < 0) {
     			hp = 0;
-    			sp -= 5;
+    			mp -= 5;
     		}
     		else {
     			hp = hp;
-    			sp -= 5;
+    			mp -= 5;
     		}
     }
     }
     public void usePotion() {
     	hp += 15;
+    	System.out.println("You used a potion!\n)"
+    			+ "Your HP is now: " + hp + "\n" +
+    			"You feel invigorated!");
     }
     public void useEther() {
-    	sp += 15;
+    	mp += 15;
+    	System.out.println("You used an ether!\n)"
+    			+ "Your MP is now: " + mp);
     }
+
 
     public boolean isDead() { return hp <= 0; }
 
@@ -60,5 +69,25 @@ public class Player{
 
     public int getHp() { return hp; }
     
-    public int getSp() { return sp; }
+    public int getMp() { return mp; }
+    
+    public void actionPerformed(ActionEvent e)
+    {
+        //Execute when button is pressed
+
+        if(e.getSource() == button1){
+            System.out.println(Hero.attack());
+            Hero.dealDamage();
+        }
+        if(e.getSource() == button2){
+            System.out.println(Hero.useMagic());
+            Hero.dealMagicDamage();
+        }
+        if(e.getSource() == button3){
+            Hero.usePotion();
+        }
+        if(e.getSource() == button4){
+            Hero.useEther();
+        }
+}
 }
